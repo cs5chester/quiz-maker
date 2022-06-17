295d155fd76eddbed907d9ffad2a3816c2f5e0ca
Nikolay Kirichenko <mkyrychenko@magecom.us>
2022-06-17 17:10:13 +0300

    build questions

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -1,33 +1,32 @@
-import React, {useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import Quiz from '../Quiz/Quiz'
+import GetStarted from "../Quiz/GetStarted";
+import '../../styles/widget.css';
 
-import './widget.css';
+function Widget({config}) {
+    const [isQuizStarted, setIsQuizStarted] = useState(false);
+    const minutes = config.quizTimeMinutes;
 
-function Widget() {
-    function addScript( src ) {
-        const script = document.createElement('script');
-        script.type = "text/javascript";
-        script.addEventListener("load", function(event) {
-            qz.setKey("A1403561515LA6471E1H5LAD298D685LA3162I9102L4753I4512LA14827176DL1906307B4");
+    if(isQuizStarted && minutes) {
+        const quizTime = +minutes * 60000;
 
-            /* Load your Quiz */
-            qz.load({
-                quiz: "QFMF6P7V5",
-                parent: ".widget-holder",
-                onCreate: function(quizDetails) {
-                    console.log(quizDetails);
-                }
-            });
-        });
-        script.src = src;
-        document.getElementsByTagName('head')[0].appendChild(script);
+        setTimeout(()=>{
+            setIsQuizStarted(false);
+        }, quizTime)
     }
-
-    useEffect(()=>{
-        addScript('https://take.quiz-maker.com/3012/CDN/quiz-embed-v1.js')
-    }, [])
-
     return (
-        <div className={'widget-holder'}></div>
+        <div className={'quiz-widget-holder'}>
+            {
+                isQuizStarted ?
+                    <Quiz config={config}/>
+                :
+                    <GetStarted
+                        setIsQuizStarted={setIsQuizStarted}
+                        quizTime={minutes}
+                    />
+
+            }
+        </div>
     )
 }
 
